265cca6b0378bfb506747f6e15943bbf4122748c
MOHAMMAD <m.ayazadeh@gmail.com>
2021-07-19 17:13:29 +0430

    Code: 28947

---
+++ msin.py
@@ -0,0 +1,40 @@
+### 28947 ###
+### Done ###
+
+m = int(input())
+
+if m == 1:
+    print('sib')
+elif m == 2:
+    print('sib')
+    print('samanoo')
+elif m == 3:
+    print('sib')
+    print('samanoo')
+    print('senjed')
+elif m == 4:
+    print('sib')
+    print('samanoo')
+    print('senjed')
+    print('serkeh')
+elif m == 5:
+    print('sib')
+    print('samanoo')
+    print('senjed')
+    print('serkeh')
+    print('sekeh')
+elif m == 6:
+    print('sib')
+    print('samanoo')
+    print('senjed')
+    print('serkeh')
+    print('sekeh')
+    print('sabzeh')
+elif m == 7:
+    print('sib')
+    print('samanoo')
+    print('senjed')
+    print('serkeh')
+    print('sekeh')
+    print('sabzeh')
+    print('sofreh')
