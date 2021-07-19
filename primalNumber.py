778592cbf2606c9c6e65493f5302cbe53debd6b0
MOHAMMAD <m.ayazadeh@gmail.com>
2021-07-19 17:10:17 +0430

    quera question

---
+++ primalNumber.py
@@ -0,0 +1,23 @@
+num = int(input())
+
+### num is primal or not
+# for i in range(2, num):
+#     if num % i == 0:
+#         print('not primal')
+#         break
+# else:
+#     print('primal')
+
+
+### number's between 1 and num are primal or not
+# def primal(n):
+#     for i in range(2, n):
+#         if n % i == 0:
+#             return False
+#     return True
+#
+#
+# for i in range(2, num+1):
+#     if primal(i) :
+#         print(i, end=' ')
+
