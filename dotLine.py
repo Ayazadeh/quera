2bec118b3e79ae68633f36283faa6470bb85696d
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-28 12:03:17 +0430

    Code: 3414

---
+++ dotLine.py
@@ -0,0 +1,11 @@
+""" 3414 """
+""" Done """
+
+x1, y1, x2, y2 = map(int, input().split(' '))
+
+if x1 == x2:
+    print('Vertical')
+elif y1 == y2:
+    print('Horizontal')
+else:
+    print('Try again')
